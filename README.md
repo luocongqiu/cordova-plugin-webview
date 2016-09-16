88d3f8684164f7f1cbaf8ebc8af5f1579f590d1f
luocongqiu <luocongqiu@gmail.com>
2016-09-16 18:42:10 +0800

    初始提交

--- README.md
+++ README.md
@@ -16,13 +16,10 @@
 - `url` 需要打开的连接地址，远程地址或本地地址
 - `name` 窗口名字，`_self` 在本页面打开, `_system` 调用系统浏览器打开，其他打开新窗口
 - `options` 配置参数
-<pre><code>
     {
         'headBarBg': '#FF7902', //标题头背景色
         'headBarHeight': 44,    //标题头高度，默认44
         'titleColor': '#FFFFFF' //标题字体颜色 用于显示页面标题
     }
-</code></pre>
-
 - `successCallback` 成功加载页面回调函数
 - `errorCallback` 打开失败回调函数
--- www/WebView.js
+++ www/WebView.js
@@ -1,67 +1,14 @@
 var exec = require('cordova/exec');
-var channel = require('cordova/channel');
 
-function WebView() {
-    this.channels = {};
-}
-
-WebView.prototype = {
-
-    _eventHandler: function (event) {
-        if (event && (event.type in this.channels)) {
-            this.channels[event.type].fire(event);
-        }
-    },
-
-    close: function () {
-        exec(null, null, 'WebView', 'close', []);
-        return this;
-    },
-
-    show: function () {
-        exec(null, null, 'WebView', 'show', []);
-        return this;
-    },
-
-    reload: function () {
-        exec(null, null, 'WebView', 'reload', []);
-        return this;
-    },
-
-    addEventListener: function (eventName, f) {
-        if (!(eventName in this.channels)) {
-            this.channels[eventName] = channel.create(eventName);
-        }
-        this.channels[eventName].subscribe(f);
-        return this;
-    },
-
-    removeEventListener: function (eventName, f) {
-        if (eventName in this.channels) {
-            this.channels[eventName].unsubscribe(f);
-        }
-        return this;
-    }
-};
-
-exports.open = function (url, name, options, callbacks) {
-
-    var webView = new WebView();
-    callbacks = callbacks || {};
-    for (var callbackName in callbacks) {
-        if (!callbacks.hasOwnProperty(callbackName)) {
-            return;
-        }
-        webView.addEventListener(callbackName, callbacks[callbackName]);
-    }
-
-    var cb = function (eventName) {
-        webView._eventHandler(eventName);
-    };
-
-    setTimeout(function () {
-        exec(cb, cb, 'WebView', 'open', [url, name, options || {}]);
-    }, 0);
-    return webView;
+/**
+ *
+ * @param url 本地或远程地址
+ * @param name 可取值 - '_self' 在当前窗口打开,'_system' 调用系统浏览器打开，其他在新窗口打开
+ * @param options 参数 可以设置标题头背景颜色，字体颜色，状态栏颜色默认为标题头颜色
+ * @param successCallback 成功回调
+ * @param errorCallback 失败回调
+ */
+exports.open = function (url, name, options, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, 'WebView', 'open', [url, name, options || {}]);
 };
 
